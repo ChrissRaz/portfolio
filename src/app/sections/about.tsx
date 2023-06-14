5b3fdce808ea0ebd437dd811a13f43d018c5f81a
Chrissraz <chrissraz22@gmail.com>
2023-06-15 01:54:10 +0300

    responsive about

--- src/app/sections/about.tsx
+++ src/app/sections/about.tsx
@@ -14,15 +14,16 @@ import nodejs from "../../../public/images/nodejs.png";
 
 const About = () => {
   return (
-    <section className="flex justify-between content-end align-bottom h-screen relative">
-      <div className=" absolute bottom-28 right-40 w-32 h-32">
+    <section className="flex flex-col md:flex-row md:justify-between md:content-end md:align-bottom h-screen relative">
+
+      <div className=" absolute bottom-28 right-16 md:right-40 w-24 h-24 md:w-32 md:h-32">
         <div className="relative w-full h-full">
-          <div className="w-16 h-16 bg-secondaire rounded absolute bottom-0 right-0"></div>
-          <div className="w-28 h-28 bg-principal rounded absolute top-0 left-0"></div>
+          <div className="w-10 h-10 md:w-16 md:h-16 bg-secondaire rounded absolute bottom-0 right-0"></div>
+          <div className="w-20 h-20  md:w-28 md:h-28 bg-principal rounded absolute top-0 left-0"></div>
         </div>
       </div>
 
-      <div className="w-1/2 h-screen relative">
+      <div className="w-0 h-0 md:w-1/2 md:h-screen relative invisible md:visible">
         <Image
           src={AboutImage}
           alt=""
@@ -31,12 +32,13 @@ const About = () => {
         />
       </div>
 
-      <div className="w-1/2 flex flex-col justify-center about-me">
+      <div className="w-[85%] h-[700px] md:h-auto ml-8 md:ml-0 md:mr-0 md:w-1/2 flex flex-col justify-center about-me">
+
         <div className=" flex flex-col justify-between max-w-xl ">
-          <div className="font-bold mb-4 text-xl">
+          <div className="font-bold mb-4 text-[18px] md:text-xl">
             My name is RAZANAMIHOATRA Jean Christian.
             <FontAwesomeIcon
-              className="text-principal text-8xl pl-3 animate-gelatineSlow"
+              className="text-principal text-5xl md:text-8xl pl-3 animate-gelatineSlow"
               icon={faQuestion}
             />
           </div>{" "}
@@ -45,9 +47,9 @@ const About = () => {
             <span className="text-principal font-semibold">passionate</span>{" "}
             about everything that is{" "}
             <span className="text-principal font-semibold">
-              digital creation
-            </span>
-            and specifically IT
+              digital creation 
+            </span>{" "}
+             and specifically IT
             {`development, that's why I can help a company or an \n
             entrepreneur to counter their projects.`}
             I specialized in{" "}
@@ -56,7 +58,7 @@ const About = () => {
             </span>{" "}
             {`but that does not limit my 
             interests for other platforms. In terms of technology, I'm more on
-            the`}
+            the `}
             <span className="text-principal font-semibold">
               Javascript/Typescript/NodeJS
             </span>{" "}
@@ -69,31 +71,31 @@ const About = () => {
             <div className="text-principal font-semibold ">
                 {/* <span className="">*</span>
                 <span>Angular</span> */}
-                <Image src={nodejs} alt="" className="w-auto h-16" />
+                <Image src={nodejs} alt="" className="w-auto h-11 md:h-16 " />
               </div>
 
               <div className="text-principal font-semibold ">
                 {/* <span className="">*</span>
                 <span>Angular</span> */}
-                <Image src={angular} alt="" className="w-auto h-12" />
+                <Image src={angular} alt="" className="w-auto  h-8  md:h-12" />
               </div>
 
               <div className="text-principal font-semibold">
                 {/* <span>*</span>
                 <span>ReactJS/NextJS</span> */}
-                <Image src={react} alt="" className="w-auto h-12" />
+                <Image src={react} alt="" className="w-auto h-8 md:h-12" />
               </div>
 
               <div className="text-principal font-semibold">
                 {/* <span>*</span>
                 <span>ExpressJS</span> */}
-                <Image src={express} alt="" className="w-auto h-12" />
+                <Image src={express} alt="" className="w-auto h-8 md:h-12" />
               </div>
 
               <div className="text-principal font-semibold">
                 {/* <span>*</span>
                 <span>NestJS</span> */}
-                <Image src={nest} alt="" className="w-auto h-12" />
+                <Image src={nest} alt="" className="w-auto h-8 md:h-12" />
               </div>
             </div>
           </div>
