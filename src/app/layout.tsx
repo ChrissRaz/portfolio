101513778e0062223f8008d71e26c05265b87499
chrissraz <chrissraz22@gmail.com>
2023-12-03 14:11:38 +0300

    update seo config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,14 @@ import "./globals.css";
 import Loading from "./loading";
 import Head from "next/head";
 import Script from "next/script";
+import { NextSeo } from 'next-seo';
 
-export const metadata = {
-  title: "Developer web - mobile - Christian",
+
+// export const metadata = {
+//   title: "Developer web - mobile - Christian",
   
-  description: "Obtenez l'expertise dont vous avez besoin pour réaliser votre projet de développement web et mobile. Profitez également de services de gestion de projets numériques efficaces.",
-};
+//   description: "Obtenez l'expertise dont vous avez besoin pour réaliser votre projet de développement web et mobile. Profitez également de services de gestion de projets numériques efficaces.",
+// };
 
 export default function RootLayout({
   children,
@@ -18,11 +20,40 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <meta name="description" content="Obtenez l'expertise dont vous avez besoin pour réaliser votre projet de développement web et mobile. Profitez également de services de gestion de projets numériques efficaces." />
+      <NextSeo
+      title="IT Poject Manager | Fullstack developer | R. Jean Christian"
+      description="Obtenez l'expertise dont vous avez besoin pour réaliser votre projet de développement web et mobile. Profitez également de services de gestion de projets numériques efficaces."
+      // canonical="https://www.canonical.ie/"
+      openGraph={{
+        url: 'https://chrissraz.com/',
+        title: 'IT Poject Manager | Fullstack developer | R. Jean Christian',
+        description: "Obtenez l'expertise dont vous avez besoin pour réaliser votre projet de développement web et mobile. Profitez également de services de gestion de projets numériques efficaces.",
+        images: [
+          {
+            url: '/images/og-image-02.png',
+            width: 800,
+            height: 600,
+            alt: 'R. Jean Christian',
+            type: 'image/png',
+          },
+          {
+            url: '/images/og-image-03.png',
+            width: 900,
+            height: 800,
+            alt: 'Og Image Alt Second',
+            type: 'image/png',
+          },
+          { url: '/images/og-image.png' },
+        ],
+        siteName: 'Razanamihoatra Jean Christian',
+      }}
+    /> 
+        {/* <meta name="description" content="Obtenez l'expertise dont vous avez besoin pour réaliser votre projet de développement web et mobile. Profitez également de services de gestion de projets numériques efficaces." /> */}
       </Head>
       <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-608164694"/>
       <Script src="https://chrissraz.com/js/google.js"/>
 
+      
       {/* <Suspense fallback={<Loading />}> */}
       <body>{children}</body>
       {/* </Suspense> */}
